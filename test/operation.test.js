551e5537c43a373ef217960a08f74f249610316c
jgurung <jgurun@pointpark.edu>
2024-04-17 01:43:54 -0400

    added path to package.json and modified the test file

--- test/operation.test.js
+++ test/operation.test.js
@@ -1,37 +1,37 @@
 // the functions to be tested with jest test framework.
 
-const { add, subtract, multiply, divide, power } = require('../../src/operation');
+const { add, subtract, multiply, divide, power } = require('../src/operation');
 
 // test case for addition
-test('add function', () => {
+describe('add function', () => {
     test('adds 1 + 2 to equal 3', () => {
         expect(add(1, 2)).toBe(3);
     });    
 });
 
 // // test case for subtraction
-test('subtract function', () => {
+describe('subtract function', () => {
     test('subtracts 2 - 1 to equal 1', () => {
         expect(subtract(2, 1)).toBe(1);
     });    
 });
 
 // test case for multiplication
-test('multiply function', () => {
+describe('multiply function', () => {
     test('multiplies 2 * 3 to equal 6', () => {
         expect(multiply(2, 3)).toBe(6);
     });   
 });
 
 // test case for division
-test('divide function', () => {
+describe('divide function', () => {
     test('divides 6 / 2 to equal 3', () => {
         expect(divide(6, 2)).toBe(3);
     });    
 });
 
 // test case for power
-test('power function', () => {
+describe ('power function', () => {
     test('calculates power of 2^3 to equal 8', () => {
         expect(power(2, 3)).toBe(8);
     });
